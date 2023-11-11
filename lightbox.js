80bfb9c29e9ff1840d843f2384d5882d53be5da3
kadiebwe oscar <kadiebweoscar@gmail.com>
2023-11-11 18:44:38 +0100

    add logic for lightbox in file lightbox.js

--- lightbox.js
+++ lightbox.js
@@ -1,15 +1,29 @@
 const imageElements = document.querySelectorAll('img[src=".png"], [src=".jpeg"], [src=".jpg"]');
+const body = document.querySelector('body');
 
 imageElements.forEach((imageElement)=>{
     imageElement.addEventListener('click', (e)=>{
         e.preventDefault()
-        getSrcOfImage(imageElement)
+        const srcOfImage = getSrcOfImage(imageElement);
+        setSrcOnImgLightBox(srcOfImage)
     })
 })
 
 
 function getSrcOfImage(element) {
     const src = element.getAttribute("src");
+    console.log(src);
     return src
 }
 
+function setSrcOnImgLightBox(srcImage){
+    const div = document.createElement('DIV');
+    div.setAttribute("class", "lightbox")
+    div.innerHTML=`<button class="lightbox__close">Fermer</button>
+    <button class="lightbox__next">Suivant</button>
+    <button class="lightbox__prev">Precedent</button>
+    <div class="lightbox__container">
+        <img src="${srcImage}" alt="">
+    </div>`
+    body.appendChild(div);
+}
